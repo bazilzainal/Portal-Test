c93a1f91950b23c3d9dedfadd8ff06e774c4c71a
Bazil Z <41966264+bazilzainal@users.noreply.github.com>
2021-05-27 15:38:12 +0800

    Remove OnTriggerStay, add Update and GhostApparate to fix bad frame and physics sync.
    Signed-off-by: Bazil Z <41966264+bazilzainal@users.noreply.github.com>

--- Assets/Scripts/PortalGhostEffect.cs
+++ Assets/Scripts/PortalGhostEffect.cs
@@ -5,23 +5,39 @@ public class PortalGhostEffect : MonoBehaviour
 
     public SpriteRenderer ghost;
     public PortalTeleporter thisPortal;
+    bool enterPortal = false;
+    Collider2D currentPortal;
+    Vector3 offset;
 
-    //Start the ghost effect when entering the portal
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        Debug.Log("Triggered!");
+        if (enterPortal)
+        {
+            GhostApparate();
+        }
+    }
 
-        var offset = collision.transform.position - transform.position;
+    void GhostApparate()
+    {
+        offset = currentPortal.transform.position - transform.position;
 
         ghost.transform.position = thisPortal.portalOther.transform.position + offset;
 
         ghost.gameObject.SetActive(true);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered!");
+        enterPortal = true;
+        currentPortal = collision;
+        
+    }
     //Disable the ghost effect when leaving the portal
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Left!");
         ghost.gameObject.SetActive(false);
+        enterPortal = false;
     }
 }
